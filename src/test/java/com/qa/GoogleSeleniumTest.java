dc8874b40778f5204fe003154e21107f7bd32f70
Chris <Christopher.Burns@academytrainee.com>
2019-09-11 10:04:19 +0100

    added test to search google

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -1,7 +1,37 @@
 package com.qa;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSeleniumTest {
     private ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\seleniumtesting\\src\\test\\java\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void searchTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("http://google.com");
+        WebElement searchBar = driver.findElement(By.name("q"));
+        assertTrue(searchBar.isDisplayed());
+        searchBar.sendKeys("funny cat pictures");
+        WebElement submitButton = driver.findElement(By.name("btnK"));
+        submitButton.click();
+        Thread.sleep(5000);
+    }
 }
