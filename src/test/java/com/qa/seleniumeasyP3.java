1dc8aa8d18a62ac6933b5ee1d3eabf2ed521493d
Chris <Christopher.Burns@academytrainee.com>
2019-09-11 13:44:58 +0100

    added test class for seleniumeasy page 3

---
+++ src/test/java/com/qa/seleniumeasyP3.java
@@ -0,0 +1,22 @@
+package com.qa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumeasyP3 {
+    ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\seleniumtesting\\src\\test\\java\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+
+}
