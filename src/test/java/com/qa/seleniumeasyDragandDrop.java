82117da887453977e161ab648f36fc1d9b861c19
Chris <Christopher.Burns@academytrainee.com>
2019-09-12 13:38:56 +0100

    renamed mouse moving test class added test

--- src/test/java/com/qa/seleniumeasyDragandDrop.java
+++ src/test/java/com/qa/seleniumeasyDragandDrop.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class MouseActionsTest {
     ChromeDriver driver;
@@ -17,5 +18,16 @@ public class MouseActionsTest {
     @After
     public void tearDown(){
         driver.close();
+    }
+
+    @Test
+    public void mouseMoveTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+        Thread.sleep(1000);
+
+        Actions action = new Actions(driver);
+        action.moveByOffset(200, 200).perform();
+        Thread.sleep(1000);
     }
 }
