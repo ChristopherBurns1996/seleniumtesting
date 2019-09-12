db087b9bf58de60167e6a44f782a7f207324bb7e
Chris <Christopher.Burns@academytrainee.com>
2019-09-12 13:50:35 +0100

    test doesnt seem to work

--- src/test/java/com/qa/seleniumeasyDragandDrop.java
+++ src/test/java/com/qa/seleniumeasyDragandDrop.java
@@ -3,10 +3,11 @@ package com.qa;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class MouseActionsTest {
+public class seleniumeasyDragandDrop {
     ChromeDriver driver;
 
     @Before
@@ -23,11 +24,15 @@ public class MouseActionsTest {
     @Test
     public void mouseMoveTest() throws InterruptedException {
         driver.manage().window().maximize();
-        driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+        driver.get("https://www.seleniumeasy.com/test/drag-and-drop-demo.html");
         Thread.sleep(1000);
 
         Actions action = new Actions(driver);
-        action.moveByOffset(200, 200).perform();
+
+        WebElement drag1 = driver.findElementByXPath("//*[@id=\"todrag\"]/span[1]");
+        WebElement dropbox = driver.findElementById("mydropzone");
+        action.moveToElement(drag1).dragAndDrop(drag1, dropbox).perform();
+        action.moveByOffset(100, 200).perform();
         Thread.sleep(1000);
     }
 }
